377068e2e8e86cd8042a5164759d6d90808d216c
kwangsooshin <kepler92@daum.net>
2018-07-03 19:05:05 +0900

    asd

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,6 +11,5 @@ from django.contrib.auth.management.commands.createsuperuser import get_user_mod
 if not get_user_model().objects.filter(username='$DJANGO_SUPERUSER_USERNAME'): 
     get_user_model()._default_manager.db_manager().create_superuser(username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD')"
 sh start_server.sh
-/bin/bash
 
 exec "$@"
