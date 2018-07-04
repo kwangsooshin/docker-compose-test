866bba30071a1ad48292fbf5e944ee3372e27f0f
kwangsooshin <kepler92@daum.net>
2018-07-04 13:21:41 +0900

    asd

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,14 +10,9 @@ django.setup()
 from django.contrib.auth.management.commands.createsuperuser import get_user_model
 if not get_user_model().objects.filter(username='$DJANGO_SUPERUSER_USERNAME'): 
     get_user_model()._default_manager.db_manager().create_superuser(username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD')"
-echo "Start Server"
 sh start_server.sh
 
-trap 'exitHandler' SIGTERM
-exitHandler() {
-    echo "Shutdown Server"
-    sh shutdown_server.sh
-}
+trap 'sh shutdown_server.sh' SIGTERM
 while true; do :; done
 
 exec "$@"
--- shutdown_server.sh
+++ shutdown_server.sh
@@ -1,3 +1,3 @@
 (exec kill $(ps aux |awk '/runserver/ {print $2}')) 2>/dev/null &
 (exec kill $(ps aux |awk '/celery/ {print $2}')) 2>/dev/null &
-echo "Stop Server"
+echo "Shutdown Server"
